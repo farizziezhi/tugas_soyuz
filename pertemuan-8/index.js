3de774b7b9838c58b10cb2efce6fc0450fd7afd0
muh.farizzi <ezhifarizzi@gmail.com>
2025-05-02 18:31:48 +0800

    This is the Eigth Meeting

---
+++ pertemuan-8/index.js
@@ -0,0 +1,72 @@
+// console.log("Hello World");
+
+// let aku = prompt("Masuk ayok: ", "Aku udah masuk");
+
+// console.log(aku);
+
+// {
+//   var bb = 0;
+//   let cc = 0;
+//   const dd = 0;
+// }
+
+// let cc = 0;
+// let dd = 0;
+
+// console.log(bb);
+// console.log(cc);
+// console.log(dd);
+
+// console.log(a);
+
+// var a = 0;
+// var a = 10.8;
+
+// console.log(a);
+
+// let x = "angga";
+// x = "aqil";
+
+// console.log(x);
+
+// let o = null;
+// console.log(typeof o);
+
+// let name = "ramdan";
+// console.log(typeof name);
+
+// if (name == "ramdan") {
+//   a += a;
+// }
+
+// console.log(a);
+
+let number = -2;
+
+if (number > 0) {
+  console.log("besar");
+} else if (number == 0) {
+  console.log("nol aku");
+} else {
+  console.log("kecil");
+}
+
+if (0) {
+  console.log("incorrect");
+}
+
+if (2) {
+  console.log("correct");
+}
+
+console.log(2 > 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("0000000001" > 2);
+console.log("10" == true);
+
+console.log(true && true);
+console.log(false || true);
+console.log(false == true);
